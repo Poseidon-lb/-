5be5354e876b3da788ea209d39fe82e34d4ae258
lb <lb@1139217246@qq.com>
2024-12-24 16:45:28 +0800

    update

--- project/ThreadPoll/ThreadPoll.h
+++ project/ThreadPoll/ThreadPoll.h
@@ -17,12 +17,20 @@ public:
     ThreadPool(int min = 2, int max = std::thread::hardware_concurrency());
     ~ThreadPool();
 
+/*
+    F 是可调用对象的类型。 Args是类型包
+    F 的类型是 int(int, int)，所以 F(Args...) 表示调用 void(int, int) 类型的函数，传递两个 int 类型的参数
+    int(int, int) 函数返回 int，所以 result_of<int(int, int)(int, int)>::type 的结果是 void
+*/
     template<class F, class...Args>
     auto pushTask(F && f, Args && ...args) -> std::future<typename std::result_of<F(Args...)>::type> {
         using reType = typename std::result_of<F(Args...)>::type;
         // std::shared_ptr<std::packaged_task<reType()>>
+        // 使用共享智能指针，防止pushTask结束后析构后，子线程中的也析构了
         auto task_ptr = std::make_shared<std::packaged_task<reType()>> (
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+            /*不知道f会有几个参数 bind消元
+            完美转发保留原来类型。f和args可能本来是右值引用，使用就变成了左值，传过去就变左值引用了*/
         );  
         {
             std::unique_lock<std::mutex> locker(_taskMutex);
